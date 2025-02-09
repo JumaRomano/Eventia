46a6499c41578d5397c208323945745666ad9237
Romano Victor <romanojuma15@gmail.com>
2025-02-09 22:37:47 +0300

    Database was created and added

--- eventia-backend/event_management/events/models.py
+++ eventia-backend/event_management/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('organizer', 'Organizer'),
+        ('attendee', 'Attendee'),
+        ('admin', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='attendee', blank=False, null=False)
+    
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
